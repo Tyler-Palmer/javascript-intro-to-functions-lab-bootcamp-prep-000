fe35dcbd6178da8b90535986fd17076740876158
Tyler W Palmer <tylerpalmer7@gmail.com>
2018-07-20 22:33:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+//shout
+function shout(string)
+{
+  return string.toUpperCase()
+}
+//whisper
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+//logShout
+function logShout(shout)
+{
+  console.log('HELLO')
+}
+//logWhisper
+function logWhisper(whisper)
+{
+  console.log('hello')
+}
+//lowerCase Grandma
+function sayHiToGrandma(string) {
+  if (string === 'hello')
+}
+}
